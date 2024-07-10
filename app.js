b12a1c93fe4e76b100c6a85f0c726efeb9019dca
Raushan raj <92861083+raushanraj99@users.noreply.github.com>
2024-07-10 21:11:20 +0530

    cors added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const country = require("./routes/country");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config({
   path:'./data/config.env'
